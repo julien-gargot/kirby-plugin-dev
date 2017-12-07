2e1af3f9aa6e8d5996a2fb09f82f08823958e9a5
Julien Gargot <julien@g-u-i.me>
2017-12-07 15:56:58 +0100

    FIX: update for less version

--- snippets/dev.php
+++ snippets/dev.php
@@ -3,16 +3,16 @@
   <div class="<?php echo c::get('dev.grid.container.name','container-fluid') ?>">
     <div id="grid" class="row">
       <?php for ($i=1; $i <= c::get('dev.grid.column.count',12); $i++) : ?>
-      <div class="col-1"><div class="in"><?= $i ?></div></div>
+      <div class="col-xs-1 col-1"><div class="in"><?= $i ?></div></div>
       <?php endfor ?>
     </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-xs-12 col-12">
         <div id="size"></div>
       </div>
     </div>
     <div class="row">
-      <div class="col col-12">
+      <div class="col-xs-12 col-12">
         <div id="width"></div>
       </div>
     </div>
