4fa734fda658d6ff6e5493b58b8e21aec9d09d09
Julien Gargot <julien@g-u-i.net>
2019-04-18 21:57:22 +0200

    Better grid display on mobile.

--- snippets/dev.php
+++ snippets/dev.php
@@ -3,7 +3,7 @@
   <div class="<?php echo c::get('dev.grid.container.name','container-fluid') ?>">
     <div id="grid" class="row">
       <?php for ($i=1; $i <= c::get('dev.grid.column.count',12); $i++) : ?>
-      <div class="col-xs-1 col-1"><div class="in"><?= $i ?></div></div>
+      <div class="col"><div class="in"><?= $i ?></div></div>
       <?php endfor ?>
     </div>
     <div class="row">
